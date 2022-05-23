660f6f2b91aa295d45f237076bafec7f39adeed1
Alexander Kukoba <luckychel@gmail.com>
2022-05-24 01:19:39 +0300

    homework1 найти гипотенузу, периметр, площадь по заданным катетам

--- homework1.playground/Contents.swift
+++ homework1.playground/Contents.swift
@@ -14,7 +14,7 @@ if (a == 0) {
 
 if (!isErr)
 {
-    print("Решить квадратное уравнение вида: " + (a > 0 ? "" : "-")+String(a)+"x2 "+(b > 0 ? "+ " : "")+String(b)+"x"+(c > 0 ? " + " : "")+String(c)+" = 0")
+    print("Задание 1 \"Решить квадратное уравнение вида " + (a > 0 ? "" : "-")+String(a)+"x2 "+(b > 0 ? "+ " : "")+String(b)+"x"+(c > 0 ? " + " : "")+String(c)+" = 0\"")
 
     let dis : Double = (b*b) - (4*a*c)
     
@@ -25,13 +25,13 @@ if (!isErr)
     if dis > 0 {
         x1 = (-b+sqrt(dis))/(2*a)
         x2 = (-b-sqrt(dis))/(2*a)
-        print("Результат - 2 корня: x1 = " + String(x1) + " и x2 = " + String(x2))
+        print("Результат - 2 корня: x1 = " + String(format: "%.2f", x1) + " и x2 = " + String(format: "%.2f", x2))
     }
     //1 корень
     else if dis == 0 {
         x1 = -b/(2 * a)
         x2 = x1
-        print("Результат - 1 корень: x1 = x2 = " + String(x1) + ".")
+        print("Результат - 1 корень: x1 = x2 = " + String(format: "%.2f", x1) + ".")
     }
     //корней нет
     else {
@@ -41,8 +41,25 @@ if (!isErr)
 
 isErr = false
 
+print("\n")
+
 //Даны катеты прямоугольного треугольника. Найти площадь, периметр и гипотенузу треугольника
 
+print("Задание 2 \"Даны катеты прямоугольного треугольника. Найти площадь, периметр и гипотенузу треугольника\"")
+var katet1 : Double = 7
+var katet2 : Double = 5
+
+var gipotenuza: Double?
+var perimetr : Double?
+var square : Double?
+
+gipotenuza = sqrt(katet1*katet1 + katet2*katet2)
+perimetr = katet1 + katet2 + (gipotenuza ?? 0)
+square = 0.5 * katet1 * katet2
+
+print("Результат: гипотенуза = " + String(format: "%.2f", gipotenuza ?? 0) + ". Периметр = " + String(format: "%.2f", perimetr ?? 0) + ". Площадь = " + String(format: "%.2f", square ?? 0) + ".")
+
+print("\n")
 
 
 
