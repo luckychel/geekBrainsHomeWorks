dff1d671872d3ad0ff87a4b8192f76ac81b5037a
Alexander Kukoba <luckychel@gmail.com>
2022-06-28 01:13:42 +0300

    не могу протянуть аутлет, в чем может быть проблема - не пойму

---
+++ vk_KukobaAlexander/vk_KukobaAlexander/Controller/Friends/CollectionView/FriendCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  FriendCollectionViewCell.swift
+//  vk_KukobaAlexander
+//
+//  Created by Александр Кукоба on 28.06.2022.
+//
+
+import UIKit
+
+class FriendCollectionViewCell: UICollectionViewCell {
+    
+}
