50b8f5161b3499afb3caaf21fea18ad170c15b1f
Alexander Kukoba <luckychel@gmail.com>
2022-06-28 00:19:02 +0300

    добавил модель группу, раскидал по папкам файлы

--- vk_KukobaAlexander/vk_KukobaAlexander/Model/Friend.swift
+++ vk_KukobaAlexander/vk_KukobaAlexander/Model/Friend.swift
@@ -4,17 +4,20 @@
 //
 //  Created by Александр Кукоба on 26.06.2022.
 //
-
 import UIKit
 
 class Friend {
     
     let name: String
     let avatar: UIImage?
+    let allFriends: [Friend]?
+    let allGroups: [Group]?
 
-    init(name: String, avatar: UIImage? = nil) {
+    init(name: String, avatar: UIImage? = nil, allFriends: [Friend]? = nil, allGroups: [Group]? = nil) {
         self.name = name
         self.avatar = avatar
+        self.allFriends = allFriends
+        self.allGroups = allGroups
     }
 
 }
---
+++ vk_KukobaAlexander/vk_KukobaAlexander/Model/Group.swift
@@ -0,0 +1,22 @@
+//
+//  Group.swift
+//  vk_KukobaAlexander
+//
+//  Created by Александр Кукоба on 28.06.2022.
+//
+
+import Foundation
+
+class Group {
+    
+    let name: String
+    let description: String
+   
+
+    init(name: String, description: String) {
+        self.name = name
+        self.description = description
+      
+    }
+
+}
