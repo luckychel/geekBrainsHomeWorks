21f0b2f68cd6b34e4a61c1c549c80d6a229b909d
Alexander Kukoba <luckychel@gmail.com>
2022-06-29 00:25:06 +0300

    ДЗ полностью готово

--- vk_KukobaAlexander/vk_KukobaAlexander/Controller/Groups/TableView/GroupsTableViewController.swift
+++ vk_KukobaAlexander/vk_KukobaAlexander/Controller/Groups/TableView/GroupsTableViewController.swift
@@ -9,9 +9,18 @@ import UIKit
 
 class GroupsTableViewController: UITableViewController {
 
+    var groups = [
+        Group(name: "Программисты C#", description: "Эта группа создана для ищущих себя в великолепном языке программирования от компании Microsoft"),
+        Group(name: "Отдых на реке с палатками", description: "Встречи, посиделки у костра, рыбалка, уха, коллективное кормление комаров, палаточный отдых, душевные беседы"),
+        Group(name: "Ремонт своими руками", description: "Специалисты этой группы подскажут вам, как добиться желаемого резщультата при минимальных затратах"),
+        Group(name: "Музыка техно, драм, дип хаус", description: "Здесь вы можете найти интересующий вас трек и скачать себе на устройство"),
+        Group(name: "Вся литература", description: "Художественная литература, научная литература, фантастика, зарубежная литература, история, стихи")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.title = "Группы"
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -23,24 +32,50 @@ class GroupsTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return groups.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "GroupCell", for: indexPath) as? GroupCell else {
+            preconditionFailure("Error")
+        }
 
-        // Configure the cell...
+        cell.groupName.text = groups[indexPath.row].name
+        cell.groupDescription.text = groups[indexPath.row].description
 
         return cell
     }
-    */
+    
+    
+    //возврат по клику на группу при добавлении на следующем экране
+    @IBAction func addSelectGroup(segue: UIStoryboardSegue) {
+        if (segue.identifier == "addGroupToAll") {
+            guard let allGroupsController = segue.source as? GroupsViewController else { return }
+            
+            if let indexPath = allGroupsController.allGroups.indexPathForSelectedRow {
+                let group = allGroupsController.groups[indexPath.row]
+                
+                if (!groups.contains(where: {$0.name == group.name})) {
+                    groups.append(group)
+                    
+                    tableView.reloadData()
+                }
+            }
+        }
 
+        //print(sourceVC)
+        //print(segue.destination)
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath:IndexPath) {
+        print("нажата строка № \(indexPath.row) в секции \(indexPath.section)")
+    }
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
@@ -49,17 +84,18 @@ class GroupsTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            groups.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
